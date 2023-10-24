fe695091d5fe01b91d74d1321596150cedc2ec91
sdavid <sundaydavid501@gmail.com>
2023-10-23 22:20:27 -0500

    Managing Multiple Firebase Projects in a React Native App

--- App.tsx
+++ App.tsx
@@ -5,12 +5,24 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 import Config from 'react-native-config';
 
 function App() {
+  useEffect(() => {
+    firestore()
+      .collection('test')
+      .add({
+        name: `Hello from ${Config.ENV}`,
+        age: 30,
+      })
+      .then(() => {
+        console.log('Data added!');
+      });
+  });
   return (
     <View style={{marginTop: 40, flex: 1}}>
       <Text>Environment: {Config.ENV}</Text>
